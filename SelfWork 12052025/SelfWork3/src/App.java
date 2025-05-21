835e884f27caf6de2fe348ac5a6421784c0b1ea4
Joseph2412 <random47@hotmail.it>
2025-05-21 18:22:01 +0200

    Creazione dell'esercizio su Prorgammazione Funzionale

---
+++ SelfWork 12052025/SelfWork3/src/App.java
@@ -0,0 +1,43 @@
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+    
+    List<Character> charaters = List.of(
+
+            new Character("Geralt", Type.WITCHER, Race.HUMAN),
+            new Character("Jennefer", Type.ASSASSIN, Race.HUMAN),
+            new Character("IronMan", Type.CYBORG, Race.HUMAN),
+            new Character("Link", Type.KNIGHT, Race.ELF),
+            new Character("Gandalg", Type.MAGE, Race.HUMAN),
+            new Character("Legolas", Type.ARCHER, Race.ELF),
+            new Character("Kharjo", Type.ASSASSIN, Race.KHAJIIT),
+            new Character("Aragon", Type.KNIGHT, Race.HUMAN),
+            new Character("Vesemir", Type.WITCHER, Race.HUMAN),
+            new Character("Ciri", Type.WITCHER, Race.HUMAN),
+            new Character("Ezio Auditore", Type.ASSASSIN, Race.HUMAN),
+            new Character("Arya Stark", Type.ASSASSIN, Race.HUMAN),
+            new Character("Robocop", Type.CYBORG, Race.HUMAN),
+            new Character("Cyborg", Type.CYBORG, Race.HUMAN),
+            new Character("Jaime Lannister", Type.KNIGHT, Race.HUMAN),
+            new Character("Merlin", Type.MAGE, Race.HUMAN),
+            new Character("Doctor Strange", Type.MAGE, Race.HUMAN),
+            new Character("Katniss Everdeen", Type.ARCHER, Race.HUMAN),
+            new Character("Green Arrow", Type.ARCHER, Race.HUMAN),
+            new Character("Zeus", Type.DIVINITY, Race.HIGHELF),
+            new Character("Ra", Type.DIVINITY, Race.HUMAN),
+            new Character("Odin", Type.DIVINITY, Race.HUMAN),
+            new Character("Thranduil", Type.KNIGHT, Race.ELF),
+            new Character("Galadriel", Type.MAGE, Race.HIGHELF),
+            new Character("Elrond", Type.MAGE, Race.HIGHELF),
+            new Character("Gimli", Type.KNIGHT, Race.DWARF),
+            new Character("Thorin", Type.KNIGHT, Race.DWARF),
+            new Character("Dwalin", Type.KNIGHT, Race.DWARF),
+            new Character("Ra'zirr", Type.ASSASSIN, Race.KHAJIIT),
+            new Character("M'aiq the Liar", Type.MAGE, Race.KHAJIIT)
+
+            
+            //ordiniamo e stampiamo le varie Liste usando alcuni filtri per tipologia
+            );
+        }
+}
---
+++ SelfWork 12052025/SelfWork3/src/Character.java
@@ -0,0 +1,37 @@
+public class Character {
+
+    private String name;
+    private Type type;
+    private Race race;
+    
+    public Character(String name, Type type, Race race) {
+        this.name = name;
+        this.type = type;
+        this.race = race;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public Race getRace() {
+        return race;
+    }
+
+    public void setRace(Race race) {
+        this.race = race;
+    }
+    
+}

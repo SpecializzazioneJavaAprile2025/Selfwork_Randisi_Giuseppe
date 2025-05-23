f9a43e63062702d3d7c2af1305fae54edba00198
Joseph2412 <63360070+Joseph2412@users.noreply.github.com>
2025-05-24 00:57:38 +0200

    SelfWork Maven ongoing....

---
+++ maven-intro/src/main/java/it/aulab/AppConfig.java
@@ -0,0 +1,9 @@
+package it.aulab;
+
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class AppConfig {
+
+}   
---
+++ maven-intro/src/main/models/Arm.java
@@ -0,0 +1,16 @@
+package it.aulab.models;
+
+public class Arm {
+    
+    private Side side;
+
+    public Side getSide() {
+        return side;
+    }
+
+    public void setSide(Side side) {
+        this.side = side;
+    }
+
+}
+//1.28.31
